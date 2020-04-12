cbd9fa8024b04b05325745fdcb5ac23ff576497b
Igor Couto <igor.fcouto@gmail.com>
2020-04-12 14:51:43 -0300

    Seach feature done

--- personas/src/App.js
+++ personas/src/App.js
@@ -5,30 +5,49 @@ import './App.css';
 const App = () => 
 {
 
-  const [people, setPerson] = useState([]);
-
-  const exampleRequest = "https://randomuser.me/api/?results=10";
+  // States
+  const [people, setPeople] = useState([]);
+  const [showingPeople, setShowingPeople] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect( () => {
-    getFaces();
+    getPeople();
   }, []);
 
-  const getFaces = async () => {
-    const response = await fetch(exampleRequest, {method: 'GET'} );
+  const getPeople = async () => {
+    const response = await fetch("https://randomuser.me/api/?results=10", {method: 'GET'} );
     const data = await response.json();
 
-    console.log(data.results);
+    setPeople(data.results);
+    setShowingPeople(data.results);
 
-    setPerson(data.results);
+    console.log(data.results);
+  };
+
+  const updateSearch = event => {
+
+    const searchedWord = event.target.value.toLowerCase();
+    setSearch(event.target.value);
+
+    const searchResult = [];
+    
+    people.forEach( person => {
+      if(person.name.first.toLowerCase().includes(searchedWord) || person.name.last.toLowerCase().includes(searchedWord))
+        searchResult.push(person);
+    }); 
+    
+    setShowingPeople(searchResult);
+
+    if(search === '') setShowingPeople(people);      
   };
 
   return(
     <div className="App">
       <form className="search-form">
-        <input className="seach-bar" type="text"/>
+        <input className="seach-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="seach-buttom" type="submit">Search</button>
       </form>
-      {people.map( person => (
+      {showingPeople.map( person => (
         <Person 
           key = {person.name.first + person.name.last}
           name = {person.name.first}
