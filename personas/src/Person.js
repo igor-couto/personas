e40aceb5c570384a0a98ac31222269346f0f17fc
Igor Couto <igor.fcouto@gmail.com>
2020-04-12 23:44:37 -0300

    layout changes

--- personas/src/Person.js
+++ personas/src/Person.js
@@ -1,5 +1,7 @@
 import React from "react";
 import style from './Person.module.css';
+import MaleIcon from './male.svg';
+import FemaleIcon from './female.svg';
 
 const Person = ({name, lastname, gender, origin, photo, wondering, feelLike, flag, state, city}) => {
     return(
@@ -8,20 +10,19 @@ const Person = ({name, lastname, gender, origin, photo, wondering, feelLike, fla
             <div className={style.photo}>
                 <object className="emb" data={photo} width="100" height="100" type="image/svg+xml"></object>
             </div>
-            <h1>{name + ' ' + lastname}</h1>
+            <h1>{name + ' ' + lastname} <object data={gender === 'female'? FemaleIcon : MaleIcon} width="18" height="18" type="image/svg+xml"></object></h1> 
 
-
-            <div>
-                <p>Origin: {origin} <img src={flag} width="32"/></p>
-                <p>State/Province: {state}</p>
-                <p>City: {city}</p>
-            </div>
-
-            <div>
-                <p>Gender: {gender}</p>
+            <div className={style.location}>
+                <div className={style.flag}>
+                    <img src={flag} width="32"/>
+                </div>
+                <div className={style.origininfo}>
+                    <p>Origin: {origin}</p>
+                    <p>State/Province: {state}</p>
+                    <p>City: {city}</p>
+                </div>
             </div>
             
-
             <p>Feeling like: {feelLike}</p>
             <p>Wondering: {wondering}</p>
         </div>
