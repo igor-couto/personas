540de3a5114b73ed99ef28654b6bea51071f12c3
Igor Couto <igor.fcouto@gmail.com>
2020-04-13 23:21:34 -0300

    Updates

---
+++ precache-manifest.bc02f14f7fca1d1b6dd4042f52ea3672.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a408df858eb84cf37beb7da8e7d3a1",
+    "url": "/personas/index.html"
+  },
+  {
+    "revision": "9931cf99413781ff8f70",
+    "url": "/personas/static/css/main.6d102d83.chunk.css"
+  },
+  {
+    "revision": "a0b8cf40f86bb0a3f2bf",
+    "url": "/personas/static/js/2.efaf986f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personas/static/js/2.efaf986f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9931cf99413781ff8f70",
+    "url": "/personas/static/js/main.5ba46d70.chunk.js"
+  },
+  {
+    "revision": "d82292b161ef9aa8395a",
+    "url": "/personas/static/js/runtime-main.fbdfd953.js"
+  },
+  {
+    "revision": "72a7e13bc9e82a3574d76a69b5fef932",
+    "url": "/personas/static/media/female.72a7e13b.svg"
+  },
+  {
+    "revision": "ce65d9da69d67d8776757d1ddcdb8c63",
+    "url": "/personas/static/media/male.ce65d9da.svg"
+  },
+  {
+    "revision": "f41f187d0894510d37eedb6362454906",
+    "url": "/personas/static/media/search-icon.f41f187d.svg"
+  }
+]);
