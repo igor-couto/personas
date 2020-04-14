097263e18ef3af00d8f0da911118ca94abf86d63
Igor Couto <igor.fcouto@gmail.com>
2020-04-14 20:58:05 -0300

    Update PersonLocation.js

--- personas/src/PersonLocation.js
+++ personas/src/PersonLocation.js
@@ -4,15 +4,15 @@ import style from "./PersonLocation.module.css";
 const PersonLocation = ({flag, origin, state, city}) => {
     return(
         <div className={style.location}>
-        <div className={style.flag}>
-            <img src={flag} width="32" alt="flag"/>
+            <div className={style.flag}>
+                <img src={flag} width="32" alt="flag"/>
+            </div>
+            <div className={style.origininfo}>
+                <p>Origin: {origin}</p>
+                <p>State/Province: {state}</p>
+                <p>City: {city}</p>
+            </div>
         </div>
-        <div className={style.origininfo}>
-            <p>Origin: {origin}</p>
-            <p>State/Province: {state}</p>
-            <p>City: {city}</p>
-        </div>
-    </div>
     );
 };
 
